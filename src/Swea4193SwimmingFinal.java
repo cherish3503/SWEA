acfa51ec2996e3b76f550300d475d48fe8717142
cherish3503 <cherish3503@gmail.com>
2026-02-02 11:22:07 +0900

    4193 fin

--- src/Swea4193SwimmingFinal.java
+++ src/Swea4193SwimmingFinal.java
@@ -18,59 +18,107 @@ public class Swea4193SwimmingFinal {
 			int N= Integer.parseInt(br.readLine());
             int[][] board = new int[N][N];
             int[] now = new int[2];
-            int[] end = new int[2];
+            int[] dest = new int[2];
             
             for(int r=0; r<N; ++r){
-                for(int c=0; c>N; ++c){
-                    st = new StringTokenizer(br.readLine());
+            	st = new StringTokenizer(br.readLine());
+                for(int c=0; c<N; ++c){
+               
                     board[r][c] = Integer.parseInt(st.nextToken());
                 }
             }
+            
+//            Arrays.stream(board).map(Arrays::toString).forEach(System.out::println);
+            
 			st = new StringTokenizer(br.readLine());
             now[0] = Integer.parseInt(st.nextToken());
             now[1] = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-            end[0] = Integer.parseInt(st.nextToken());
-            end[1] = Integer.parseInt(st.nextToken());
+            dest[0] = Integer.parseInt(st.nextToken());
+            dest[1] = Integer.parseInt(st.nextToken());
             
+            System.out.println("#"+(test_case)+" " + swimFinal(board, now[0], now[1], dest[0], dest[1]));
            
 		}
 	}
     
-    private static int move(int[][] board, int startR, int startC){
+    private static int swimFinal(int[][] board, int startR, int startC, int destR, int destC){
         int N = board.length;
-    	int[][] minBoard = new int[N][N];
-    	for (int i = 0; i < N; i++) {
-    	    Arrays.fill(minBoard[i], -1);
-    	}
+    	boolean[][] visited = new boolean[N][N]; //false
+//    	for (int i = 0; i < N; i++) {
+//    	    Arrays.fill(minBoard[i], -1);
+//    	}
         int[][] d = {{1,0},{-1,0},{0,1},{0,-1}};
         
-        
         Deque<int[]> dq = new ArrayDeque<>();
+        
+        visited[startR][startC] = true;
+        dq.add(new int[]{startR, startC, 0});
+        
         while(!dq.isEmpty()) {
+//        	dq.stream().map(Arrays::toString).forEach(System.out::println);
+//        	System.out.println();
+        	
         	int[] now = dq.poll();
         	int nowR = now[0];
         	int nowC = now[1];
         	int cnt = now[2];
+        	
+            if(nowR == destR && nowC == destC) {
+            	return cnt;
+            }
+            
             for(int i=0, size = d.length; i<size; ++i){
                 int nr = nowR + d[i][0];
                 int nc = nowC + d[i][1];
                 if(nr < 0 || nc < 0 || nr >= N || nc >= N) {
                 	continue;
                 }
-                if(minBoard[nr][nc] != -1) {
+                if(visited[nr][nc]) {
+                	continue;
+                }
+                if(board[nr][nc] == 1) {
                 	continue;
                 }
                 
-//                dq.offer(null)
+//                System.out.println("현" + Arrays.toString(now));
+                if(board[nr][nc] == 2) {
+
+                	if(cnt%3 != 2) { // 제자리에서 기다림
+//                      visited[nowR][nowC] = false;
+                        dq.add(new int[]{nowR, nowC, cnt+1});
+                        continue;
+                	}
+//                	else {
+//                		visited[nowR][nowC] = true;
+//                	}
+                }
+
+                visited[nr][nc] = true;
+                dq.add(new int[]{nr, nc, cnt+1});
 
             }
         }
         
 
         // todo: 도착 불가 체크
-        return 0;
+        return -1;
     }
     
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
